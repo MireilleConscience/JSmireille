7c85dd814d70af251d9468d82b697032c5943f41
MireilleConscience <mireille.conscience@gmail.com>
2020-03-24 20:38:56 +0100

    primer commit

---
+++ tests/prueba1.js
@@ -0,0 +1,11 @@
+let numberOfEssai=10;
+
+console.log(typeof(numberOfEssai));
+numberOfEssai = 'bidule';
+
+let answer = true;
+answer.opposite= false;
+console.log(answer.opposite);
+
+null=10;
+console.log(null);
